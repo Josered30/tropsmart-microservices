ed0ba805d843ad7c543066ec78fc6df8092c1a57
Jose Aparcana <josered30@hotmail.com>
2021-05-29 12:38:32 -0500

    .

--- userservice/src/main/java/com/softper/userservice/client/ConfigurationClient.java
+++ userservice/src/main/java/com/softper/userservice/client/ConfigurationClient.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient("CONFIG-SERVICE")
+@FeignClient("config-service")
 @RequestMapping("api/configurations")
 public interface ConfigurationClient {
     
